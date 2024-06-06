d3721e7c8a48756a4e896dc20459afba2873d5f6
DiegoAaronMP <daaron.macias@gmail.com>
2024-06-06 13:08:46 -0600

    Crear baraja de cartas

---
+++ assets/js/juego.js
@@ -0,0 +1,30 @@
+
+/**
+ * 2C = Two of Clubs (Tréboles)
+ * 2D = Two of Diamonds (Diamantes)
+ * 2H = Two of Hearts (Corazones)
+ * 2S = Two of Spades (Espadas)
+ */
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+const crearDeck = () => {
+    for (let i = 2; i < 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for (const tipo of tipos) {
+        for (const especial of especiales) {
+            deck.push(especial + tipo);
+        }
+    }
+    
+    deck = _.shuffle(deck);
+    console.log(deck);
+}
+
+crearDeck();
